3c388e7bb557e26560e4406213af5d1e8d70a011
Fredrik <fredrikdn@gmail.com>
2020-10-21 18:25:33 +0200

    ys

--- src/components/MapboxGLMap.js
+++ src/components/MapboxGLMap.js
@@ -17,7 +17,7 @@ const MapboxGLMap = () => {
     const initializeMap = ({ setMap, mapContainer }) => {
       const map = new mapboxgl.Map({
         container: mapContainer.current,
-        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+        style: "mapbox://styles/mapbox/dark-v11", // stylesheet location
         center: [10.408773,63.422091],
         zoom: 10
       });
