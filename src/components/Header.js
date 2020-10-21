669cdd36895ff3182862b0b7d71bf22542cf1bd5
Fredrik <fredrikdn@gmail.com>
2020-10-21 17:45:20 +0200

    hes

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const styles = {
 const Header = () => {
     return (
         <div style={styles}>
-            <header>KART? KART. Kart... AKDAK</header>
+            <header>KART? KART. Kart..</header>
         </div>
     )
 }
